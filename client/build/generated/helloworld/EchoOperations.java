94c88294b716a3cae8c7c019833d5d5a4ec11f67
root <root@martindebian>
2016-04-13 17:22:55 +0200

    added example from prof borko

---
+++ client/build/generated/helloworld/EchoOperations.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+
+/**
+ * Generated from IDL interface "Echo".
+ *
+ * @author JacORB IDL compiler V 3.7
+ * @version generated at Apr 8, 2016 3:52:19 PM
+ */
+
+public interface EchoOperations
+{
+	/* constants */
+	/* operations  */
+	java.lang.String echoString(java.lang.String mesg);
+}
